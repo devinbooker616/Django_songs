1da48faf680c22f76f4aa14bec49726c3f92877f
Base Camp <basecamp@ssa-406.local>
2019-11-07 15:27:32 -0600

    added views

--- app/views.py
+++ app/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Album
+
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, "album_list.html", {"albums": albums})
+
+def album_detail(request, id):
+    album = Album.objects.get(id=id)
+    return render(request, "album_detail.html", {"album":album})
+
+def new_song(request):
+    song = Song.objects.create()
