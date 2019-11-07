f980e7db83a92dda64f332edf00da2a169a5c0d3
Base Camp <basecamp@ssa-406.local>
2019-11-07 15:24:56 -0600

    Added classes Album and Song

--- app/models.py
+++ app/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Album(models.Model):
+    title = models.TextField()
+    artist_name = models.TextField()
+
+class Song(models.Model):
+    title = models.TextField()
+    seconds = models.IntegerField()
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
